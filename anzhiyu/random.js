bfcc423fbd232dbb975acc1cd2f64cdbab8dd32f
sunwugong1 <rgwuerling@gmail.com>
2025-05-15 15:28:14 +0800

    Site updated: 2025-05-15 15:28:14

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/05/15/这是一篇新的博文/","2025/05/14/手机/","2025/05/13/骗骗喜欢你/","2025/05/13/唐探1900/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
